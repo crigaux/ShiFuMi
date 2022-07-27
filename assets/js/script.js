e494f4bb2aeb4279b925b5be22f47481f5616eff
crigaux <crigaux057@gmail.com>
2022-07-27 16:03:42 +0200

    ajout choix personnage

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,78 @@ let shifumi = (playerChoice) => {
     return result;
 }
 
+let shifumiWin = (playerChoice) => {
+    let computerChoice = Math.floor(Math.random()*5) + 1;
+
+    if(playerChoice == 1) {
+        if(computerChoice < 3) {
+            computerChoice = 2;
+        } else {
+            computerChoice = 5;
+        }
+    } else if(playerChoice == 2) {
+        if(computerChoice < 3) {
+            computerChoice = 3;
+        } else {
+            computerChoice = 4;
+        }
+    } else if(playerChoice == 3) {
+        if(computerChoice < 3) {
+            computerChoice = 1;
+        } else {
+            computerChoice = 5;
+        }
+    } else if(playerChoice = 4) {
+        if(computerChoice < 3) {
+            computerChoice = 3;
+        } else {
+            computerChoice = 1;
+        }
+    } else {
+        if(computerChoice <= 3) {
+            computerChoice = 2;
+        } else {
+            computerChoice = 4;
+        }
+    }
+
+    switch (computerChoice) {
+        case 1:
+            document.querySelector('.computerChoiceImg').setAttribute('src', './assets/img/rock.jpg');
+            break;
+        case 2:
+            document.querySelector('.computerChoiceImg').setAttribute('src', './assets/img/paper.jpg');
+            break;
+        case 3:
+            document.querySelector('.computerChoiceImg').setAttribute('src', './assets/img/scissors.jpg');
+            break;
+        case 4:
+            document.querySelector('.computerChoiceImg').setAttribute('src', './assets/img/lizard.jpg');
+            break;
+        case 5:
+            document.querySelector('.computerChoiceImg').setAttribute('src', './assets/img/spock.jpg');
+            break;
+        default:
+            break;
+    }
+    document.querySelector('.answer > span').style.opacity='1';
+    document.querySelector('.answer > span').textContent = 'Gagné';
+    document.querySelector('.answer > span').style.backgroundColor = '#66cc00';
+    document.querySelector('.answer > span').style.borderColor = '#4d9900';
+    return 'win';
+}
+// Choix du joueur
+
+let player;
+
+document.querySelectorAll('.chooseYourPlayer').forEach(element => {
+    element.addEventListener('click', () => {
+        player = element.getAttribute('id');
+        document.querySelector('.playerChoiceImg').src = element.src;
+        document.querySelector('#playerChoiceMain').classList.add('active');
+    })
+})
+
 let played = document.querySelector('.played');
 let win = document.querySelector('.win');
 let loose = document.querySelector('.loose');
@@ -56,11 +128,17 @@ let winrate = document.querySelector('.winrate');
 
 let playerChoiceFunction = (id, index, src) => {
     document.querySelector(id).addEventListener('click', () => {
+        console.log(player);
         document.querySelector('.playerChoiceImg').setAttribute('src', src);
-        if(shifumi(index) == 'win') {
+        if(player == 'sheldon') {
+            shifumiWin(index);
             win.innerHTML = parseInt(win.innerHTML) + 1;
-        } else if(shifumi(index) == 'loose') {
-            loose.innerHTML = parseInt(loose.innerHTML) + 1;
+        }else {
+            if(shifumi(index) == 'win') {
+                win.innerHTML = parseInt(win.innerHTML) + 1;
+            } else if(shifumi(index) == 'loose') {
+                loose.innerHTML = parseInt(loose.innerHTML) + 1;
+            }
         }
         played.innerHTML = parseInt(played.innerHTML) + 1;
         winrate.innerHTML = Math.floor((parseInt(win.innerHTML) / parseInt(played.innerHTML)) * 100) + '%';
@@ -97,24 +175,16 @@ document.querySelector('.stats > input').addEventListener('click', () => {
     document.querySelector('.statOverlay').classList.add('statOverlayIn');
 })
 
+
 // Bouton reset
 
 document.querySelector('.reset > input').addEventListener('click', () => {
     document.querySelector('.answer > span').style.opacity='0';
-    document.querySelector('.computerChoiceImg').setAttribute('src', './assets/img/Sheldon.webp');
-    document.querySelector('.playerChoiceImg').setAttribute('src', './assets/img/kripke.jpg');
+    document.querySelector('.computerChoiceImg').setAttribute('src', './assets/img/kripke.png');
+    document.querySelector('.playerChoiceImg').src = document.getElementById(player).src;
     played.innerHTML = 0; 
     win.innerHTML = 0; 
     loose.innerHTML = 0;
     winrate.innerHTML = 0;
 })
 
-// Curseur
-
-const customCursor = document.querySelector('.customCursor');
-
-window.addEventListener('mousemove', handleCustomCursor);
-
-function handleCustomCursor(e) {
-    customCursor.style.transform = `translate(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%))`
-}
